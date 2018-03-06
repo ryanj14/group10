3eea625f5f0e95ac4d2540c231c33619a4a8b65e
Ryan <datmanwich@gmail.com>
2018-03-05 21:20:31 -0800

    WaitingList page function working, and added a table for the page

--- eleos.sql
+++ eleos.sql
@@ -1,20 +1,27 @@
 CREATE TABLE Calculator 
 (
-    id              INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+    id              INT(6)      UNSIGNED AUTO_INCREMENT PRIMARY KEY
     ,growType       VARCHAR(30) NOT NULL
-    ,numAcres       INT(255)  NOT NULL
-    ,vegtables      INT(255)  NOT NULL
-    ,orchards       INT(255)  NOT NULL
-    ,berries        INT(255)  NOT NULL
-    ,vineyards      INT(255)  NOT NULL
-    ,herbs          INT(255)  NOT NULL
-    ,otherCult      INT(255)  NOT NULL
+    ,numAcres       INT(255)    NOT NULL
+    ,vegtables      INT(255)    NOT NULL
+    ,orchards       INT(255)    NOT NULL
+    ,berries        INT(255)    NOT NULL
+    ,vineyards      INT(255)    NOT NULL
+    ,herbs          INT(255)    NOT NULL
+    ,otherCult      INT(255)    NOT NULL
     ,hire           VARCHAR(5)  NOT NULL
     ,workHire       INT(5)
     ,workHours      INT(50)
-    ,annualBudget   INT(255)  NOT NULL
-    ,workExpense    INT(255)  NOT NULL
-    ,machineExpense INT(255)  NOT NULL
-    ,phytoExpense   INT(255)  NOT NULL
-    ,otherExpense   INT(255)  NOT NULL
+    ,annualBudget   INT(255)    NOT NULL
+    ,workExpense    INT(255)    NOT NULL
+    ,machineExpense INT(255)    NOT NULL
+    ,phytoExpense   INT(255)    NOT NULL
+    ,otherExpense   INT(255)    NOT NULL
+)
+
+CREATE TABLE WaitingList
+(
+    id          INT(6)      UNSIGNED AUTO_INCREMENT PRIMARY KEY
+    ,firstName  VARCHAR(30) NOT NULL
+    ,email      VARCHAR(30) NOT NULL
 )

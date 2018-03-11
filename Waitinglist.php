a734677bf77e0b62094349ce18f770177f8f8db1
Maggie Wu <maggiejieyao@gmail.com>
2018-03-10 17:38:52 -0800

    Maybe becuase the mergin some problem, my branch is behind the develop branch, so manually updated the files

--- Waitinglist.php
+++ Waitinglist.php
@@ -49,7 +49,7 @@
             <h1>Header</h1>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
-          
+
         <div class="userWait">
             <form name="waitForm" action="" onsubmit="return validateForm()" method="post">
                 Name:<br>
@@ -60,34 +60,29 @@
                 <input id="calSubmit" type="submit" name="submit2" value="Submit">
             </form>
         </div>
-          
+
         <?php
             if(isset($_POST['submit2']))
             {
                 $firstName = $_POST['firstName'];
                 $email = $_POST['email'];
-                
                 // Connecting to the database
                 $list = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
                 // If we don't connect to the database it will spit out an error for us to fix
                 if(!$list)
                 {
                     die("Connection failed: ".mysqli_connect_error()); // Remove the connect_error method after done testing because of hacking issues.
                 }
-                
                 $sql = "INSERT INTO WaitingList(id, firstName, email) VALUES(NULL, '$firstName', '$email')";
-                
                 // Checking to see if we actually placed the data into the database
-                if (mysqli_query($list, $sql)) 
+                if (mysqli_query($list, $sql))
                 {
                     echo "New record created successfully<br>";
-                } 
-                else 
+                }
+                else
                 {
                     echo "Error: " . $sql . "<br>" . mysqli_error($list). "<br>";
                 }
-
                 // Closing the connection to the database
                 mysqli_close($list);
                 header("Refresh:0");
@@ -103,34 +98,27 @@
                 <?php
                     // Connecting to the database
                     $list = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-                
                     $row = mysqli_query($list, "SELECT * FROM WaitingList");
-                
                     if ($result = $list->query("SELECT * FROM WaitingList")) {
-
                         /* determine number of rows result set */
                         $row_cnt = $result->num_rows;
-
                         printf("Result set has %d rows.\n", $row_cnt);
-
                         /* close result set */
                         $result->close();
                     }
-                
-                
                     while($sqlRow = mysqli_fetch_assoc($row)){
-                    ?>   
+                    ?>
                 <tr>
                     <td><?php echo $sqlRow['firstName']; ?></td>
                     <td><?php echo $sqlRow['email']; ?></td>
-                </tr>                 
-                <?php 
+                </tr>
+                <?php
                     }
                     mysqli_close($list);
                 ?>
             </table>
         </div>
-          
+
     </div>
 
         <!---Footer Start--->
@@ -152,7 +140,7 @@
               <tr >
                 <th><a href="TechnologyPage.html">Technology</a></th>
                 <th><a href="Waitinglist.php">Waiting List</a></th>
-                <th><a href="#">About us</a></th>
+                <th><a href="Aboutus.html">About us</a></th>
                 <th><a href="ContactUs.html">Contact Us</a></th>
               </tr>
               <tr>
@@ -203,7 +191,7 @@
           </div>
         </footer>
     </div>
-    
+
     <script>
         function validateForm() {
             var x = document.forms["waitForm"]["firstName"].value
@@ -224,8 +212,7 @@
                 return false;
             }
         }
-        
-        function ValidateEmail(mail) 
+        function ValidateEmail(mail)
         {
             var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
             if(mail.value.match(mailformat))

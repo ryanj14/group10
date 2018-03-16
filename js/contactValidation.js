52db171b415b4cb62d2d8342c2cece21aa134743
Ryan <datmanwich@gmail.com>
2018-03-16 09:14:30 -0700

    Added the semi-colons to the end of each statements involving boolean functions

--- js/contactValidation.js
+++ js/contactValidation.js
@@ -20,36 +20,42 @@ function validateForm() {
 }
 
 function validateName() {
-    var name = document.forms["contactForm"]["name"].value
-    if (name == "") {
-      document.getElementById("nameWarning").style.visibility = 'visible'
+    var name = document.forms["contactForm"]["name"];
+    var nameValue = document.forms["contactForm"]["name"].value;
+    if (nameValue == "") {
+        name.focus();
+      document.getElementById("nameWarning").style.visibility = 'visible';
       return false;
     } else {
-      document.getElementById("nameWarning").style.visibility = 'hidden'
+      document.getElementById("nameWarning").style.visibility = 'hidden';
     }
     return true;
 }
 
 function validateEmail() {
-    var emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    var email = document.forms["contactForm"]["email"].value
+    var emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var email = document.forms["contactForm"]["email"];
+    var emailValue = document.forms["contactForm"]["email"].value;
 
-    if (!(email.match(emailPattern))) {
-      document.getElementById("emailWarning").style.visibility = 'visible'
+    if (!(emailValue.value.match(emailPattern))) {
+    email.focus();
+      document.getElementById("emailWarning").style.visibility = 'visible';
       return false;
     } else {
-      document.getElementById("emailWarning").style.visibility = 'hidden'
+      document.getElementById("emailWarning").style.visibility = 'hidden';
       return true;
     }
 }
 
 function validateTextArea() {
-    var text = document.forms["contactForm"]["inquiry"].value
-    if(text == "") {
-      document.getElementById("textWarning").style.visibility = 'visible'
+    var text = document.forms["contactForm"]["inquiry"];
+    var textValue = document.forms["contactForm"]["inquiry"].value;
+    if(textValue == "") {
+        text.focus();
+      document.getElementById("textWarning").style.visibility = 'visible';
       return false;
     } else {
-      document.getElementById("textWarning").style.visibility = 'hidden'
+      document.getElementById("textWarning").style.visibility = 'hidden';
     }
     return true;
 }
